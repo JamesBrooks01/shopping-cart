0c333d867396f6c240bcfce62b561c7bacb56649
James Brooks <brooksjamesk01@gmail.com>
2022-02-24 17:20:31 -0800

    added cart funtionality

--- js/cart.js
+++ js/cart.js
@@ -5,6 +5,7 @@
 const table = document.getElementById('cart');
 table.addEventListener('click', removeItemFromCart);
 let cart;
+let bodyRow = table.getElementsByTagName('tbody')
 
 function loadCart() {
   const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
@@ -19,7 +20,11 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  // for (let i = 0; i < cart.items.length; i++) {
+  //   bodyRow.deleteRow(-1);
+  // }
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
@@ -29,6 +34,19 @@ function showCart() {
   // TODO: Iterate over the items in the cart
   // TODO: Create a TR
   // TODO: Create a TD for the delete link, quantity,  and the item
+  for(let i = 0; i < cart.items.length; i++) {
+    let itemRow = document.createElement('tr');
+    bodyRow[0].appendChild(itemRow);
+    let deleteLink = document.createElement('td');
+    deleteLink.textContent = ('X');
+    itemRow.appendChild(deleteLink);
+    let quantity = document.createElement('td');
+    quantity.textContent = cart.items[i].quantity;
+    itemRow.appendChild(quantity);
+    let item = document.createElement('td');
+    item.textContent = cart.items[i].product;
+    itemRow.appendChild(item);
+  }
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
 
 }
